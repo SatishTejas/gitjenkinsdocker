5f09bde9c1bc09a051c1de4c94f18a6f5b48f0b8
Satish <tejassatish@hotmail.com>
2023-03-02 16:17:10 +0530

    First

---
+++ src/main/java/com/example/jenkinsdocker/ui/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.jenkinsdocker.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@GetMapping
+	public String getName() {
+
+		return "Ajay";
+	}
+
+}
